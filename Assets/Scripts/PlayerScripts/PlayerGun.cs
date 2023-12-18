927593ca329c376e6e192843e9febaca94b3be80
nico233 <1601550204@qq.com>
2023-12-18 09:40:29 +0800

    适配枪口朝向根据鼠标位置改变

--- Assets/Scripts/PlayerScripts/PlayerGun.cs
+++ Assets/Scripts/PlayerScripts/PlayerGun.cs
@@ -1,13 +1,15 @@
 using System;
 using Guns;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace PlayerScripts
 {
     public class PlayerGun : MonoBehaviour
     {
         [SerializeField] private GameObject gunPrefab;
-        
+        [SerializeField] private Player player;
+
         private IGun _gun;
         private GameObject _currentGun;
 
@@ -28,6 +30,14 @@ namespace PlayerScripts
             ShowGun();
         }
 
+        private void Update()
+        {
+            if (gunPrefab != null)
+            {
+                HandleGunDirection();
+            }
+        }
+
         /// <summary>
         /// 将 gunPrefab 挂载到
         /// </summary>
@@ -37,10 +47,34 @@ namespace PlayerScripts
             {
                 return;
             }
+
             _currentGun = Instantiate(gunPrefab, transform);
             _gun = _currentGun.GetComponent<IGun>();
             _currentGun.transform.localPosition =
                 new Vector3(_gun.GunPara.transformOffset.x, _gun.GunPara.transformOffset.y, 0);
+        }
+
+        /// <summary>
+        /// 根据鼠标位置处理枪械朝向
+        /// </summary>
+        private void HandleGunDirection()
+        {
+            var mousePos = GameInput.Instance.GetMousePosInWorldPoint();
+            var direction = (mousePos -
+                             new Vector2(_currentGun.transform.position.x,_currentGun.transform.position.y)).normalized;
+            _currentGun.transform.right = direction;
+            HandleGunFlip(direction);
+        }
+
+        /// <summary>
+        /// 处理枪械位置翻转
+        /// </summary>
+        private void HandleGunFlip(Vector2 direction)
+        {
+            int flipX = player.IsFacingRight ? 1 : -1;
+            int flipY = direction.x > 0f ? 1 : -1;
+
+            _currentGun.transform.localScale = new Vector3(flipX, flipY, _currentGun.transform.localScale.z);
         }
     }
 }

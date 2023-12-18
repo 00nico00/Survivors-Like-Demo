aae998b39a30584ac1eda4443cada1b0fad17818
nico233 <1601550204@qq.com>
2023-12-18 17:09:50 +0800

    添加 PistolBullet

---
+++ Assets/Scripts/Bullets/PistolBullet.cs
@@ -0,0 +1,19 @@
+﻿using ScriptableObjects;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Bullets
+{
+    public class PistolBullet : MonoBehaviour, IBullet, IAttackable
+    {
+        [SerializeField] private BulletConfigSO bulletConfig;
+
+        public BulletConfigSO BulletConfig => bulletConfig;
+
+
+        public void Attack(IDamageable target)
+        {
+            target.TakeDamage(bulletConfig.bulletDamage);
+        }
+    }
+}
